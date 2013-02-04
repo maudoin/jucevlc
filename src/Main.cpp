5421e255843d256086d39969b31437d849c0d89f
ma <devnull@localhost>
2013-02-04 23:45:16 +0100

    Embedded font

--- src/Main.cpp
+++ src/Main.cpp
@@ -21,15 +21,44 @@ String getFileNameWithoutExtension(const String& fullPath)
         return fullPath.substring (lastSlash);
 }
 
+Typeface* loadFont( String inPath)
+{
+	File fontFile;
+	if (File::isAbsolutePath (inPath))
+	{
+		fontFile = File(inPath);
+	}
+	else
+	{
+		fontFile = File::getCurrentWorkingDirectory().getChildFile(inPath);
+	}
+
+	FileInputStream ins(fontFile);
+	return (new CustomTypeface (ins));
+
+}
+
 class LnF : public OldSchoolLookAndFeel
 {
-
+	ScopedPointer<Typeface> cFont;
 public:
 	LnF()
 	{
 		setColour(DirectoryContentsDisplayComponent::textColourId, Colours::white);
-
+		
+		//cFont = loadFont( "ForgottenFuturistShadow.bin");
 	}
+		const Typeface::Ptr getTypefaceForFont (const Font &font)
+		{
+			if (cFont)
+			{
+				return (cFont);
+			}
+			else
+			{
+				return (font.getTypeface());
+			}
+		}
 	const Font getFontForTextButton (TextButton& button)
 	{
 		Font f = LookAndFeel::getFontForTextButton(button);
@@ -98,8 +127,8 @@ void drawFileBrowserRow (Graphics& g, int width, int height,
     }
 	
 	Font f = g.getCurrentFont();
-	f.setTypefaceName("Times New Roman");
-	f.setStyleFlags(f.getStyleFlags() | Font::FontStyleFlags::bold);
+	f.setTypefaceName("Forgotten Futurist Shadow"/*"Times New Roman"*/);
+	f.setStyleFlags(Font::FontStyleFlags::plain);
 	g.setFont(f);
 
     g.setColour (findColour (DirectoryContentsDisplayComponent::textColourId));
@@ -300,10 +329,62 @@ private:
 
 //==============================================================================
 
+
+void serializeFont(String fontName, String out, uint32 glyphCount=256)
+{
+	
+	File fontFile;
+	if (File::isAbsolutePath (out))
+	{
+		fontFile = File(out);
+	}
+	else
+	{
+		fontFile = File::getCurrentWorkingDirectory().getChildFile(out);
+	}
+		
+
+	if (!fontFile.hasWriteAccess ())
+	{
+		printf ("initialise ERROR can't write to destination file: %s\n", fontFile.getFullPathName().toUTF8());
+		return;
+	}
+
+	if (fontName == String::empty)
+	{
+		printf ("initialise ERROR no font name given\n");
+		return;
+	}
+
+
+	printf ("Fserialize::serializeFont looking for font in system list [%s]\n", fontName.toUTF8());
+	
+	Array <Font> systemFonts;
+	Font::findFonts (systemFonts);
+	for (int i=0; i<systemFonts.size(); i++)
+	{
+		if (systemFonts[i].getTypeface()->getName() == fontName)
+		{
+			CustomTypeface customTypefacePlain;
+
+			customTypefacePlain.addGlyphsFromOtherTypeface (*(systemFonts[i].getTypeface()), 0, glyphCount);
+			
+			FileOutputStream streamPlain(fontFile);
+			customTypefacePlain.writeToStream (streamPlain);
+		}
+	}
+
+	printf ("Fserialize::serializeFont finished\n");
+
+}
+
 	
 static juce::JUCEApplicationBase* juce_CreateApplication() { return new JUCEHelloWorldApplication(); } 
 extern "C" JUCE_MAIN_FUNCTION 
 { 
+	//serializeFont("Forgotten Futurist Shadow", "ForgottenFuturistShadow.bin", 1024);
+
+
     juce::JUCEApplication::createInstance = &juce_CreateApplication; 
 	try
 	{
