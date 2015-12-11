fa3725e19a3a01d61ca52a4e7216ecbd0da5768c
ma <devnull@localhost>
2015-12-11 02:07:14 +0100

    guid pin filter was broken!!

--- juce/modules/juce_video/native/juce_win32_DirectShowComponent.cpp
+++ juce/modules/juce_video/native/juce_win32_DirectShowComponent.cpp
@@ -81,7 +81,7 @@ IPin *GetPin(IBaseFilter *pFilter, PIN_DIRECTION PinDir, const GUID* guid=NULL)
 
         if(PinDir == PinDirThis)
         {
-            if(guid == NULL)
+            if(true || guid == NULL)
             {
                 bFound = true;
             }
